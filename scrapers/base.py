ab5f0d2267956dcd44c906754409ca085aebfb56
Andrew Sou <aksou@uci.edu>
2025-07-09 02:04:57 -0700

    Started GitHub Scraper

--- scrapers/base.py
+++ scrapers/base.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 
-class base(ABC):
-    @abstractmethod
+class ScraperBase(ABC):
     def __init__(self):
         pass
     
--- scrapers/github.py
+++ scrapers/github.py
@@ -1,7 +1,14 @@
-from scrapers import base
+from scrapers import ScraperBase
+import requests
+from bs4 import BeautifulSoup
 
-class github(base):
+class GitHubScraper(ScraperBase):
     def __init__(self):
         pass
+
+    @classmethod
+    def scrape():
+        url = "insert_url"
+        
 
     
