138bdfe86054527f235a29509f26b0caba18e689
Andrew Sou <andrewsou4@gmail.com>
2025-07-15 02:02:19 -0700

    Added the frame of code with basic functionality of returning correct scraped data

--- scrapers/github.py
+++ scrapers/github.py
@@ -24,15 +24,18 @@ class GITHUBScraper(ScraperBase):
         job_tables = doc.find_all("table")
         job_data = []
 
+        date_scraped = datetime.now().strftime("%d %b %Y")
+        time_scraped = datetime.now().strftime("%H:%M")
+
         for table in job_tables[1:]:
             tbody = table.find("tbody")
             rows = tbody.find_all("tr")
 
             prev_company = None
             for row in rows:
-                current_job_data = []
                 cols = row.find_all("td")
-
+                #insert date check here so don't have to have all the extra memory/time running to add to list and just dont add if so
+    
                 current_job_data = [col.get_text(strip=True) for i, col in enumerate(cols) if i != 3]
 
                 # checks if post date is within 0-2 days
@@ -42,13 +45,35 @@ class GITHUBScraper(ScraperBase):
                     else:
                         current_job_data[0] = prev_company
 
-                    job_info = tuple(current_job_data)
+
+                    company = current_job_data[0]
+                    role = current_job_data[1]
+                    location = current_job_data[2]
+                    link = None
+                    date_posted = None
+                    time_posted = None
+                    level = None
+
+                    job_info = (
+                        None,
+                        company,
+                        role,
+                        location,
+                        link,
+                        date_posted,
+                        time_posted,
+                        date_scraped,
+                        time_scraped,
+                        "GitHub",
+                        level
+                    )
                     job_data.append(job_info)
 
-        for i in job_data:
-            print(i)
-            print()
+        # for i in job_data:
+        #     print(i)
+        #     print()
         # print(job_data)
+        return job_data
 
     @staticmethod
     def filter_date(job_data) -> bool:
