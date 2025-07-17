3ea7c67869cc742246f20d8f8483e86bb97a7d4f
jaydent4 <jaydentbid@gmail.com>
2025-07-16 18:23:57 -0700

    Fixed bad_embed()

--- embed.py
+++ embed.py
@@ -6,6 +6,7 @@ def bad_embed(msg) -> discord.Embed:
     fries = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnRoeTFkN3ZrMGNoOXprczI1cWM5NWE2bGRocjU0bGs4cDkwMG1zeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ReqpJ6fApNvOw/giphy.gif"
     embed = discord.Embed(description=description, color=discord.Color.blue())
     embed.set_image(url=fries)
+    return embed
 
 def embed(query:tuple) -> discord.Embed:
     id = replace_none(query[Columns.ID.value])
