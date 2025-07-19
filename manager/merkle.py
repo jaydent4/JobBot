cdcc73e7277ea3f18f6af6653c5e3ffac0f85298
jaydent4 <jaydentbid@gmail.com>
2025-07-19 02:30:49 -0700

    Added compare

--- manager/merkle.py
+++ manager/merkle.py
@@ -1,7 +1,18 @@
 import hashlib
 
 def hash(data):
-    return hashlib.sah256(data).hexdigest()
+    return hashlib.sha256(data).hexdigest()
+
+def compare(root1, root2):
+    if not root1 and not root2:
+        return True
+    if not root1 or not root2:
+        return False
+    if hashlib.compare_digest(bytes.fromhex(root1.hash), bytes.fromhex(root2.hash)):
+        return True
+    left = compare(root1.left, root2.left)
+    right = compare(root1.right, root2.right)
+    return left or right
 
 class MerkleTreeNode:
     def __init__(self, data):
